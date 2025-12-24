b6e2eb7578e36ed79d8173733928a9437a77ea0f
Yoav-Fuchs <fuchs.yoav@campus.technion.ac.il>
2025-12-24 19:39:38 +0200

    changed unit tests for backend #80

---
+++ backend/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert" // We will use this library for easier assertions
+)
+
