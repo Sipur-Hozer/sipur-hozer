120a80bb3439746e5ff5abfe905c6371211deb01
Noya-de-Levie <noya.de@campus.technion.ac.il>
2025-12-31 13:13:30 +0200

    adding validation to sign up.

--- backend/main.go
+++ backend/main.go
@@ -213,9 +213,35 @@ func setupRouter() *gin.Engine {
 			return
 		}
 
-		hashedPassword, err := hashPassword(req.Password)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
+		
+		// Check if user already exists
+		var existingUser AddUserRequest
+		result := db.Where("phone = ?", req.Phone).First(&existingUser)
+		if result.Error == nil {
+			c.JSON(http.StatusConflict, gin.H{"error": "User with this phone number already exists"})
+			return
+		}
+
+		// Check if the user name contains letters only
+		for _, ch := range req.FullName {
+			if !unicode.IsLetter(ch) && ch != ' ' {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Full name must contain Hebrew letters only"})
+				return
+			}
+		}
+		
+				
+		// Check if the user's phone number contains digits only
+		for _, ch := range req.Phone {
+			if !unicode.IsDigit(ch) && ch != ' ' {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Phone number must contain digits only"})
+				return
+			}
+		}
+
+		// Check if the user name contains 10 digits
+		if len(req.Phone) != 10 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Phone number must be exactly 10 digits"})
 			return
 		}
 
